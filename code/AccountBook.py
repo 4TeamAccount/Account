34a44a9e0450be1229916b8e7e9cc7b1b0185171
forybh <forybh@naver.com>
2021-04-16 00:47:40 +0900

    Create AccountBook.py

---
+++ code/AccountBook.py
@@ -0,0 +1,145 @@
+import fileinput
+import sys
+
+class CLIController:
+    @staticmethod
+    #Account class의 getAllTag()를 통해 가져온 dict를 출력
+    def printAllTag(dict):
+        print("==========태그목록============")
+        m = 1
+        s = 1
+        for key in dict.keys():
+            print(f"{m}  {key}")
+            for val in dict[key]:
+                print(f"\t{m}.{s}  {val}")
+                s += 1
+            m += 1
+            s = 1
+    #주 태그를 골랐을 시 하위 태그까지 출
+    def printSubTag(self, dict, mainTag):
+        m = 1
+        for key in dict.keys():
+            if key == mainTag:
+                break
+            m += 1
+
+        print("==========부태그==========")
+        print(f"{m} {maintag}")
+        s = 1
+        for val in dict[mainTag]:
+            print(f"{m}.{s} {val}")
+            s += 1
+
+
+class Account:
+    #파일에서 5번째 줄 읽어와 Dict 구조로 태그 목록 return
+    def getAllTag(self, account_num):
+        tagDict = {}
+        file_name = account_num + ".txt"
+        file = open(file_name, 'r')
+        for i in range(4):
+            file.readline()
+            if i == 3:
+                l = file.readline()
+        file.close()
+        sl = l.split(" ")
+        for s in sl:
+            temp = []
+            tags = s.replace("(", " ").replace(")", "").replace("/", " ").replace("\n", "").split(" ")
+            for i in range(1, len(tags)):
+                temp.append(tags[i])
+            tagDict[tags[0]] = temp
+        return tagDict
+
+    def addTag(self, dict):
+        print("태그를 추가할 위치와 태그 이름을 입력하세요")
+        tag_num, tag_name = input().replace(']', '').split('[')
+        print(tag_num, tag_name)
+        child_num = 0
+        if '.' in tag_num:
+            super_num, child_num = map(int, tag_num.split('.'))
+        else:
+            super_num = int(tag_num)
+            if super_num <= len(dict):
+                print(".!!오류 : 해당 위치에 태그가 이미 존재 합니다.")
+                self.addTag(self, dict)
+        temp = 1
+        for key in dict.keys():
+            if dict[key] == ['']:
+                dict[key] = []
+            if temp == super_num:
+                if len(dict[key]) >= child_num:
+                    print(child_num, len(dict[key]), dict[key])
+                    print(".!!오류 : 해당 위치에 태그가 이미 존재 합니다.")
+                    self.addTag(self, dict)
+            if tag_name == key:
+                print(".!!오류 : 해당 이름의 태그가 이미 존재 합니다.")
+                self.addTag(self, dict)
+                break
+            for val in dict[key]:
+                if tag_name == val:
+                    print(".!!오류 : 해당 이름의 태그가 이미 존재 합니다.")
+                    self.addTag(self, dict)
+                    break
+            temp += 1
+        if ' ' in tag_name or '\t' in tag_name:
+            print(".!!오류 : 태그 이름에는 탭이나 개행 문자가 포함될 수 없습니다.")
+            self.addTag(self, dict)
+        # 특수 문자와 숫자로만 이루어 졌을때랑 길이 넘을때 오류 넣어야함
+        print(super_num, child_num)
+        if child_num != 0:
+            temp = 0
+            for key in dict.keys():
+                temp += 1
+                if temp == super_num:
+                    dict[key].append(tag_name)
+                    break
+        else:
+            dict[tag_name] = []
+        print(dict)
+        new_tag = ""
+
+        for key in dict.keys():
+            new_tag += key + '('
+            temp = 1
+            if len(dict[key]) == 0:
+                new_tag += ')'
+            for val in dict[key]:
+                if temp == len(dict[key]):
+                    new_tag += val + ')'
+                else:
+                    new_tag += val + '/'
+                temp += 1
+            new_tag += " "
+        new_tag = new_tag[:-1]
+        new_tag += '\n'
+        for line in fileinput.input("394028.txt", inplace=True):
+            if '(' in line and '/' in line and ')' in line:
+                line = line.replace(line, new_tag)
+            sys.stdout.write(line)
+        print(new_tag)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    f = open('394028.txt', '+r')
+    l = f.readlines()
+    account = '394028'
+    # CLIController.printAllTag(Account.getAllTag(Account, account))
+    # print(len(Account.getAllTag(Account, account)))
+    Account.addTag(Account, Account.getAllTag(Account, account))
+    # print(l)
