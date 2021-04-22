909ae3b21d4e02ae902ff7cd4e9fb1af2a9b85b3
forybh <forybh@naver.com>
2021-04-22 18:02:57 +0900

    deleteTag, addTag 수정

--- code/AccountBook.py
+++ code/AccountBook.py
@@ -88,9 +88,13 @@ class CLIController:
 
 class Account:
     #파일에서 5번째 줄 읽어와 Dict 구조로 태그 목록 return
+    account_file = ""
+
     def getAllTag(self, account_num):
+
         tagDict = {}
         file_name = account_num + ".txt"
+        self.account_file = file_name
         file = open(file_name, 'r', encoding='UTF-8')
 
         for i in range(4):
@@ -103,7 +107,8 @@ class Account:
             temp = []
             tags = s.replace("(", " ").replace(")", "").replace("/", " ").replace("\n", "").split(" ")
             for i in range(1, len(tags)):
-                temp.append(tags[i])
+                if tags[i] != '':
+                    temp.append(tags[i])
             tagDict[tags[0]] = temp
         return tagDict
 
@@ -169,68 +174,141 @@ class Account:
             new_tag += " "
         new_tag = new_tag[:-1]
         new_tag += '\n'
-        for line in fileinput.input("394028.txt", inplace=True, encoding='UTF-8'):
+        for line in fileinput.input(self.account_file, inplace=True):
             if '(' in line and '/' in line and ')' in line:
                 line = line.replace(line, new_tag)
             sys.stdout.write(line)
         print(new_tag)
 
+    def deleteTag(self, dict):
+        print(dict)
+        print("삭제할 태그 위치를 입력하세요")
+        tag_num = input()
+        for c in tag_num:
+            if ord(c) < 48 and ord(c) >57 and ord(c) != 46:
+                print(".!!오류 : 태그 위치에는 숫자와 . 만 입력 가능합니다.")
+                self.deleteTag(dict)
+
+        if tag_num.count('.') > 1:
+            print(".!!오류 : 태그 위치에는 숫자와 하나의 . 만 입력 가능합니다.")
+            self.deleteTag(dict)
+
+        child_num = 0
+        if '.' in tag_num:
+            super_num, child_num = map(int, tag_num.split('.'))
+        else:
+            super_num = int(tag_num)
+            if super_num > len(dict):
+                print(".!!오류 : 해당 위치에 태그가 존재하지 않습니다.")
+                self.deleteTag(dict)
+        #하위태그는 입력 안했을때
+        if child_num == 0:
+            #삭제하려는 태그의 하위태그가 존재할때
+            print(super_num, child_num)
+            if len(dict[list(dict.keys())[super_num-1]]) != 0:
+                print(".!!오류 : 해당 위치에 태그에 하위 태그들이 존재하면 삭제 할 수 없습니다.")
+                self.deleteTag(dict)
+            else:
+                del dict[list(dict.keys())[super_num-1]]
+        #하위태그 삭제할때
+        else:
+            del dict[list(dict.keys())[super_num-1]][child_num-1]
+
+        new_tag = ""
+
+        for key in dict.keys():
+            new_tag += key + '('
+            temp = 1
+            if len(dict[key]) == 0:
+                new_tag += ')'
+            for val in dict[key]:
+                if temp == len(dict[key]):
+                    new_tag += val + ')'
+                else:
+                    new_tag += val + '/'
+                temp += 1
+            new_tag += " "
+        new_tag = new_tag[:-1]
+        new_tag += '\n'
+        for line in fileinput.input(self.account_file, inplace=True):
+            if '(' in line and '/' in line and ')' in line:
+                line = line.replace(line, new_tag)
+            sys.stdout.write(line)
+        print("...태그 삭제가 완료되었습니다.")
+        print(new_tag)
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
 if __name__ == "__main__":
-    while True:
-        #1.무결성 검사 후 실패시 리턴
-        select_sign=CLIController.login_menu()
-        
-        # 기획서상 이 부분의 목업이 따로 존재하지 않고, 로그인 회원가입등의 명령어가 없기 때문에 그냥 메뉴창에서 선택한다고 가정
-        #일단 목업이 없기에 임의로 출력하겠습니다(로그인)
-        if select_sign==1:
-            
-            while True:
-                input("ID:")
-                input("PW:")
-                #로그인에 대한 함수 return 1 -> 아이디와 파일 무결성 검사 통과 0->불통 
-                check=1
-                if check==1:
-                    break
-                else:
-                    sys.exit()
-            #계좌목록 함수 *밑은 임의의 출력입니다.
-            CLIController.accout_list()
-            #계좌 선택
-            #인자와의 구분?? manage 메뉴가 필요한가
-            while True:
-                select_account=CLIController.account_manage_menu()
-                if select_account==1:#계좌가 존재하지 않으면 다시 반복
-                    print("계좌선택함수")#존재하면1 아니면0 
-                    account_exist=1
-                    if account_exist==1:
-                        CLIController.account_function()
-                    #존재하지 않으면 반복문 다시
 
-                elif select_account==2:
-                    print("계좌생성함수")
-                    #계좌목록 출력 함수 임의의 출력
-                    print("=======================계좌 목록 출력======================\n계좌번호 \t잔고\n383902 \t500,000,000,000\n123412 \t123,456\n321432 \t3,000")
-                elif select_account==3:
-                    #다른계좌본호를 안다고 가정
-                    print("권한 요청 함수")
-                    #성공리턴 ->1 실패리턴 ->0
-                    authority=1
-                    if authority==1:
-                        print("권한요청성공")
-                    elif authority==0:
-                        print("권한요청실패")
-        elif select_sign==2:
-
-            print("회원가입 완료")
-            CLIController.account_function()
-            #계좌선택 클래스
-        elif select_sign==3:
-            print("종료")
-            sys.exit()
-            break
-        else:
-            print("올바른 인자를 입력해주세요")
+    a = Account()
+    CLIController.printAllTag(a.getAllTag('394028'))
+    # a.addTag(a.getAllTag('394028'))
+    a.deleteTag(a.getAllTag('394028'))
+    # while True:
+    #     #1.무결성 검사 후 실패시 리턴
+    #     select_sign=CLIController.login_menu()
+    #
+    #     # 기획서상 이 부분의 목업이 따로 존재하지 않고, 로그인 회원가입등의 명령어가 없기 때문에 그냥 메뉴창에서 선택한다고 가정
+    #     #일단 목업이 없기에 임의로 출력하겠습니다(로그인)
+    #     if select_sign==1:
+    #
+    #         while True:
+    #             input("ID:")
+    #             input("PW:")
+    #             #로그인에 대한 함수 return 1 -> 아이디와 파일 무결성 검사 통과 0->불통
+    #             check=1
+    #             if check==1:
+    #                 break
+    #             else:
+    #                 sys.exit()
+    #         #계좌목록 함수 *밑은 임의의 출력입니다.
+    #         CLIController.accout_list()
+    #         #계좌 선택
+    #         #인자와의 구분?? manage 메뉴가 필요한가
+    #         while True:
+    #             select_account=CLIController.account_manage_menu()
+    #             if select_account==1:#계좌가 존재하지 않으면 다시 반복
+    #                 print("계좌선택함수")#존재하면1 아니면0
+    #                 account_exist=1
+    #                 if account_exist==1:
+    #                     CLIController.account_function()
+    #                 #존재하지 않으면 반복문 다시
+    #
+    #             elif select_account==2:
+    #                 print("계좌생성함수")
+    #                 #계좌목록 출력 함수 임의의 출력
+    #                 print("=======================계좌 목록 출력======================\n계좌번호 \t잔고\n383902 \t500,000,000,000\n123412 \t123,456\n321432 \t3,000")
+    #             elif select_account==3:
+    #                 #다른계좌본호를 안다고 가정
+    #                 print("권한 요청 함수")
+    #                 #성공리턴 ->1 실패리턴 ->0
+    #                 authority=1
+    #                 if authority==1:
+    #                     print("권한요청성공")
+    #                 elif authority==0:
+    #                     print("권한요청실패")
+    #     elif select_sign==2:
+    #
+    #         print("회원가입 완료")
+    #         CLIController.account_function()
+    #         #계좌선택 클래스
+    #     elif select_sign==3:
+    #         print("종료")
+    #         sys.exit()
+    #         break
+    #     else:
+    #         print("올바른 인자를 입력해주세요")
 
