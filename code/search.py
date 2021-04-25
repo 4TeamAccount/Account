8cf01ddae690b585f1e73075c6b9dd15f4913823
yunjeongiya <yunjeongiragu@gmail.com>
2021-04-25 17:04:00 +0900

    검색기능까지 완료

--- code/search.py
+++ code/search.py
@@ -1,23 +1,28 @@
-testlist1=["2021.02.", "2021.03."]
-testlist2=["[책]", "[간식]"]
+testlist1=["2021.01", "2021.04"]
+testlist2=["[공부]", "[간식]"]
 
 class SearchResult:
-    def __init__(self, accountnum, userid):
+    def __init__(self, accountnum, userid, search_date_list, search_tag_list):
         self.accountnum=accountnum
         self.file=open('..\\'+accountnum+'.txt', encoding='UTF-8') #'ANSI'로 하니까 안되네요...
-        for i in range(6):
-            self.file.readline()
+        for i in range(5):
+            l=self.file.readline()
             if i==2:
-                self.usertype = 1 #아이디 찾아서 권한 넣는 함수 넣기(주프롬프트에서 사용한거 그대로 넣으면 될것같아요!)
-            if i==5:
+                self.usertype = 2 #l에서 아이디 찾아서 권한 넣는 함수 넣기(주프롬프트에서 사용한거 그대로 넣으면 될것같아요!)
+            if i==4:
                 self.changes=self.file.readlines() #내역들을 저장한 리스트
         self.file.close()
         self.days=[]
-        for change in changes:
+        for change in self.changes:
             d = change.split(' ')[-2]
             self.days.append(d)
-        print(days)
-    '''
+        #run
+        match_list=self.search_change(search_date_list, search_tag_list)
+        if(self.usertype==1 or self.usertype==2):
+            select_change=self.select_change(match_list)
+            if(select_change!=None):
+                self.after_find_menu(select_change)
+            
     def search_change(self, search_date_list, search_tag_list): #태그, 날짜 구분해 두 리스트로 받아오기
         date_match_set=set()
         tag_match_set=set()
@@ -25,21 +30,26 @@ class SearchResult:
         if len(search_date_list)==1:
             for i, v in enumerate(self.changes):
                 if k in v:
-                    date_match_set.add(i)
-                    
+                    date_match_set.add(i)        
         elif len(search_date_list)==2:
-            self.changes
-            sort(search_date_list)
+            search_date_list.sort()
+            start=-1
+            end=len(self.days)-1
+            
+            if len(search_date_list[1])<8:
+                search_date_list[1]+="12.31."
+            elif len(search_date_list[1])<10:
+                search_date_list[1]+="31."
+            
             for i, day in enumerate(self.days):
-                if search_date_list[0]<1:
-            
-            search_date_list[1]
-            
-            for k in search_date_list:
-                for i, v in enumerate(self.days):
-                    if k in v:
-                        date_match_set.add(i)
-
+                if start==-1:
+                    if search_date_list[0]<day:
+                        start=i
+                else:
+                    if search_date_list[1]<day:
+                        end=i-1
+            for i in range(start,end+1):
+                date_match_set.add(i)
         else:
             print("날짜는 두개만 가능.... 여기서 예외처리해도되나")
                     
@@ -48,16 +58,19 @@ class SearchResult:
                 if k in v:
                     tag_match_set.add(i)
 
-        match_set=(date_match_set & tag_match_set)
+        if(len(search_date_list)!=0 and len(search_tag_list)!=0):
+            match_set=(date_match_set & tag_match_set)
+        else:
+            match_set=(date_match_set | tag_match_set)
         match_list = sorted(match_set)
+        j=1
+        for i in match_list:
+                print(j, self.changes[i])
+                j+=1
         return match_list
 
     def select_change(self, search_list):
         while(True):
-            j=1
-            for i in search_list:
-                print(j, self.changes[i])
-                j+=1
             select = int(input("AccountNum> 내역 선택(취소는 0)> "))
             if select == 0:
                 print("주프롬프트로 돌아감")
@@ -68,10 +81,10 @@ class SearchResult:
                 print("..! 존재하지 않는 번호입니다. 범위 내의 정수만 입력할 수 있습니다.")
 
     def after_find_menu(self,selected_change):
+        print("선택된 내역: ", selected_change)
+        print("..! 원하시는 기능을 입력하세요(숫자 하나)")
+        print("1.내역 수정    2. 내역 삭제     3. 취소")
         while(True):
-            print("..! 원하시는 기능을 입력하세요(숫자 하나)")
-            print("1.내역 수정    2. 내역 삭제     3. 취소")
-            print("선택된 내역: ", selected_change)
             select = int(input("AccountNumber> 검색 후 작업 > "))
             if select==1:
                 print("수정 메뉴")
@@ -82,9 +95,10 @@ class SearchResult:
             if select==3:
                 return
             else:
-                print("..! 1~3중 원하는 기능의 메뉴 번호를 입력하세요.")
-'''
+                print("..! 1~3 중 원하는 기능의 메뉴 번호를 입력하세요.")
+                print("1.내역 수정    2. 내역 삭제     3. 취소")
+                print("선택된 내역: ", selected_change)
+
 #test
 if __name__ == '__main__':
-    search1=SearchResult()
-   # search1.after_find_menu(search1.select_change(search1.search_change(testlist1,testlist2)))
+    SearchResult("394028", "id",testlist1,testlist2)
