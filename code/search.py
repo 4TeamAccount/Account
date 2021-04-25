437bd060f95b8a0e7d30012763ba84def348f68b
yunjeongiya <yunjeongiragu@gmail.com>
2021-04-25 15:11:26 +0900

    txt파일 최신화

--- code/search.py
+++ code/search.py
@@ -2,21 +2,46 @@ testlist1=["2021.02.", "2021.03."]
 testlist2=["[책]", "[간식]"]
 
 class SearchResult:
-    def __init__(self):
-        self.file=open('..\\394028.txt', encoding='UTF-8') #'ANSI'로 하니까 안되네요...
+    def __init__(self, accountnum, userid):
+        self.accountnum=accountnum
+        self.file=open('..\\'+accountnum+'.txt', encoding='UTF-8') #'ANSI'로 하니까 안되네요...
         for i in range(6):
             self.file.readline()
+            if i==2:
+                self.usertype = 1 #아이디 찾아서 권한 넣는 함수 넣기(주프롬프트에서 사용한거 그대로 넣으면 될것같아요!)
             if i==5:
                 self.changes=self.file.readlines() #내역들을 저장한 리스트
-    
-    def searchChange(self, search_date_list, search_tag_list): #태그, 날짜 구분해 두 리스트로 받아오기
+        self.file.close()
+        self.days=[]
+        for change in changes:
+            d = change.split(' ')[-2]
+            self.days.append(d)
+        print(days)
+    '''
+    def search_change(self, search_date_list, search_tag_list): #태그, 날짜 구분해 두 리스트로 받아오기
         date_match_set=set()
         tag_match_set=set()
 
-        for k in search_date_list:
+        if len(search_date_list)==1:
             for i, v in enumerate(self.changes):
                 if k in v:
                     date_match_set.add(i)
+                    
+        elif len(search_date_list)==2:
+            self.changes
+            sort(search_date_list)
+            for i, day in enumerate(self.days):
+                if search_date_list[0]<1:
+            
+            search_date_list[1]
+            
+            for k in search_date_list:
+                for i, v in enumerate(self.days):
+                    if k in v:
+                        date_match_set.add(i)
+
+        else:
+            print("날짜는 두개만 가능.... 여기서 예외처리해도되나")
                     
         for k in search_tag_list:
             for i, v in enumerate(self.changes):
@@ -33,9 +58,9 @@ class SearchResult:
             for i in search_list:
                 print(j, self.changes[i])
                 j+=1
-            select = int(input("내역 선택(취소는 0)> "))
+            select = int(input("AccountNum> 내역 선택(취소는 0)> "))
             if select == 0:
-                print("종료")
+                print("주프롬프트로 돌아감")
                 return
             elif 0<select<=len(search_list):
                 return self.changes[search_list[select-1]]
@@ -47,7 +72,7 @@ class SearchResult:
             print("..! 원하시는 기능을 입력하세요(숫자 하나)")
             print("1.내역 수정    2. 내역 삭제     3. 취소")
             print("선택된 내역: ", selected_change)
-            select = int(input("검색 후 작업 > "))
+            select = int(input("AccountNumber> 검색 후 작업 > "))
             if select==1:
                 print("수정 메뉴")
                 break
@@ -58,7 +83,8 @@ class SearchResult:
                 return
             else:
                 print("..! 1~3중 원하는 기능의 메뉴 번호를 입력하세요.")
-
+'''
 #test
-search1=SearchResult()
-search1.after_find_menu(search1.select_change(search1.searchChange(testlist1,testlist2)))
+if __name__ == '__main__':
+    search1=SearchResult()
+   # search1.after_find_menu(search1.select_change(search1.search_change(testlist1,testlist2)))
